62bf6785d02256601effc506c36e8552151b3453
habibadahir <zermmxubi@gmail.com>
2026-02-05 21:50:47 +0300

    Update user.h

--- src/user.h
+++ src/user.h
@@ -1,1 +1,23 @@
+#include <string>
+using namespace std;
 
+class User {
+private:
+    int id;                      
+    string name;                   
+    vector<string> borrowBooks; 
+
+public:
+    User(int id, string name);
+
+    int getId();
+    string getName();
+    vector<string> getborrowBooks();
+
+
+    void setName(string n);
+  
+    bool hasBorrowed(const string &isbn);      
+    void borrowBook(const string &isbn);       
+    void returnBook(const string &isbn);
+};
