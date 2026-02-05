d5899e57d699c41abbf6086b63aa3687c6b75fd1
habibadahir <zermmxubi@gmail.com>
2026-02-05 21:40:28 +0300

    Update book.h

--- src/book.h
+++ src/book.h
@@ -1,1 +1,36 @@
 
+#include <string>
+using namespace std;
+
+class Book{
+private:
+  string title;
+  string author;
+  string isbn;
+  int publishedYear;
+  int totalCopies;
+  int availableCopies;
+
+public:
+  Book(string t, string a, string i, int year, int copies, int available);
+
+  string getTitle();
+  string getAuthor();
+  string getISBN();
+  int getPublishedYear();
+  int getTotalCopies();
+  int getAvailableCopies();
+
+  void setTitle(string t);
+  void setAuthor(string a);
+  void setISBN(string i);
+  void setPublishedYear(int year);
+  void setTotalCopies(int copies);
+  void setAvailableCopies(int available);
+
+
+  bool borrowBook();
+  bool returnBook();
+      
+};
+
