6ec44f7d3328be0a84f72f0413ae102be1ab190e
habibadahir <zermmxubi@gmail.com>
2026-02-05 21:50:14 +0300

    Update user.cpp

--- src/user.cpp
+++ src/user.cpp
@@ -1,1 +1,44 @@
+#include "User.h"
 
+User::User(int id, string name) {
+    this->id = id;
+    this->name = name;
+
+}
+
+int User::getId() {
+    return id;
+}
+string User::getName() {
+    return name; 
+}
+vector<string> User::getBorrowedBooks() { 
+    return borrowedBooks; 
+}
+void User::setName(string n) {
+    name = n;
+}
+
+bool User::hasBorrowed(string bookTitle) {
+    for (int i = 0; i < borrowedBooks.size(); i++) {
+        if (borrowedBooks[i] == bookTitle) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void User::borrowBook(string bookTitle) {
+    if (!hasBorrowed(bookTitle)) {
+        borrowedBooks.push_back(bookTitle);
+    }
+}
+
+void User::returnBook(string bookTitle) {
+    for (int i = 0; i < borrowedBooks.size(); i++) {
+        if (borrowedBooks[i] == bookTitle) {
+            borrowedBooks.erase(borrowedBooks.begin() + i);
+            break;
+        }
+    }
+}
