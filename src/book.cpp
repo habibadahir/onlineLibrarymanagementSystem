0e17260e49634867cd32f626a04e6b78d7a060b0
habibadahir <zermmxubi@gmail.com>
2026-02-05 21:39:25 +0300

    Update book.cpp

--- src/book.cpp
+++ src/book.cpp
@@ -1,1 +1,45 @@
 
+#include "Book.h"
+
+Book::Book(string t, string a, string i, int year, int copies, int available){
+  title = t;
+  author = a;
+  isbn = i;
+  publishedYear = year;
+  totalCopies = copies;
+  availableCopies = available;
+  
+}
+string Book::getTitle() { return title; }
+string Book::getAuthor() { return author; }
+string Book::getISBN() { return isbn; }
+int Book::getPublishedYear() { return publishedYear; }
+int Book::getTotalCopies() { return totalCopies; }
+int Book::getAvailableCopies() { return availableCopies; }
+
+void Book::setTitle(string t) { title = t; }
+void Book::setAuthor(string a) { author = a; }
+void Book::setISBN(string i) { isbn = i; }
+void Book::setPublishedYear(int year) { publishedYear = year; }
+void Book::setTotalCopies(int copies) { totalCopies = copies; }
+void Book::setAvailableCopies(int available) { availableCopies = available; }
+
+
+
+bool Book::borrowBook(){
+  if (availableCopies > 0){
+    availableCopies--;
+    return true;
+  }
+  return false;
+  
+}
+
+bool Book::returnBook(){
+  if (availableCopies < totalCopies){
+    availableCopies++;
+    return true;
+  }
+  return false;
+  
+}
